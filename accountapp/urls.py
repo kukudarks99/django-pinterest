f0644322e0cb578c8fe04cf35a649f558ec0bca9
Taehyong Koo <2256kth@gmail.com>
2021-11-12 17:38:51 -0500

    first commit

---
+++ accountapp/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from accountapp.views import hello_world
+
+app_name = "accountapp"
+
+urlpatterns = [
+    path('hello/', hello_world, name="hello_world")
+]
