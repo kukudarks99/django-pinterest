20e48298568285b584576c95459bec190e73c1c2
Taehyong Koo <2256kth@gmail.com>
2021-11-12 17:44:49 -0500

    settings.py add eve_file path

--- config/settings.py
+++ config/settings.py
@@ -19,9 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env(
     # set casting, default value
-    DEBUG=(bool, False),
-    env_file=os.path.join(BASE_DIR, '.env')
+    DEBUG=(bool, False)
 )
+
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
